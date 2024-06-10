f8b869b6b4c8308e07878a9327a51c51c82017f5
carpereira <59072054+carpereira@users.noreply.github.com>
2024-06-10 17:17:29 -0300

    Update graficos.py
    Implementação do gráfico

--- graficos.py
+++ graficos.py
@@ -2,3 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import numpy as np
+
+# Plot confusion matrix
+def plot_confusion_matrix(conf_matrix):
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='d', 
+                xticklabels=nv.classes_, yticklabels=nv.classes_)
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.show()
