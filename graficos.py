7c42fc5f321883436d6f92406bd5a627e95317b6
carpereira <59072054+carpereira@users.noreply.github.com>
2024-06-10 21:41:31 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -7,12 +7,13 @@ import numpy as np
 def plot_confusion_matrix(conf_matrix, classes):
     plt.figure(figsize=(8, 6))
     sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='d', 
-                xticklabels=nv.classes_, yticklabels=nv.classes_)
+                xticklabels=classes, yticklabels=classes)
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
     plt.show()
 
 nv_classes_ = ['Class 1', 'Class 2', 'Class 3']
+plot_confusion_matrix(conf_matrix, nv_classes_)
 
 
