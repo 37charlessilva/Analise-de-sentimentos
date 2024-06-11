3008533e02035dfd57550a913088fe58d0c6566e
carpereira <59072054+carpereira@users.noreply.github.com>
2024-06-10 21:12:45 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -4,14 +4,16 @@ import seaborn as sns
 import numpy as np
 
 # Plot confusion matrix
-def plot_confusion_matrix(conf_matrix):
+def plot_confusion_matrix(conf_matrix, classes):
     plt.figure(figsize=(8, 6))
     sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='d', 
-                xticklabels=nv.classes_, yticklabels=nv.classes_)
+                xticklabels=classes, yticklabels=classes)
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
     plt.show()
 
+nv_classes_ = ['Class 1', 'Class 2', 'Class 3']
+
 # Plot confusion matrix for the current classifier
-plot_confusion_matrix(conf_matrix)
+plot_confusion_matrix(conf_matrix, nv_classes_)
