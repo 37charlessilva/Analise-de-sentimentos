e742d5b8dd318265c32633ec9af64c9ac8bbb443
carpereira <59072054+carpereira@users.noreply.github.com>
2024-06-12 21:17:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,3 @@
-"""from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from dados import Dados
-from naive_bayes import Naive_bayes
-
-# nome das bases de dados
-bases = ['Bases/b2w.csv', 'Bases/buscape.csv', 'Bases/olist.csv', 'Bases/utlc_apps.csv']
-
-# inicializa a classe Naive_bayes
-nv = Naive_bayes()
-
-while(True):
-    # Menu inicial
-    print("Escolha sua base de dados\n"
-        "1: B2w\n"
-        "2: Buscape\n"
-        "3: Olist\n" 
-        "4: Utlc_apps")
-    i = int(input("Resposta: "))
-
-    # Carregar os dados a partir de um arquivo CSV
-    df = Dados(bases[i - 1])
-
-    # Separar as características (X) e os rótulos (y)
-    nv.start(df.transform_data('review_text_processed'), 
-            y = df.get_rating())
-
-    print('\nConjuntos de treinamento e teste separados!')
-    print('Tamanho do conjunto de treinamento:', nv.get_train_x().shape)
-    print('Tamanho do conjunto de teste:', nv.get_test_x().shape)
-
-    # Avaliar o desempenho do modelo
-    accuracy = nv.accuracy_score()
-    report = nv.classification_report()
-    conf_matrix = nv.confusion_matrix()
-
-    print('\nAcurácia do modelo:', accuracy)
-    print('\nRelatório de classificação:')
-    print(report)
-    print('\nMatriz de confusão:')
-    print(conf_matrix)
-    print("\n")
-
-    nv_classes_ = df.get_classes()  # Assuming you have a method in Dados class to get class names
-    plot_confusion_matrix(conf_matrix, nv_classes_)
-    
-    # Plot confusion matrix for the current classifier
-    print(plot_confusion_matrix(conf_matrix, nv_classes_))"""
-
-#import sys
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from naive_bayes import Naive_bayes
@@ -85,9 +31,6 @@ while True:
     # Treinar modelo Naive Bayes
     nv.start(X_train, y_train)
 
-    
-    #z = df.get_head()
-    #print(z)
     print(df.get_head())
     print('\nConjuntos de treinamento e teste separados!')
     print()
@@ -108,6 +51,6 @@ while True:
     print('\nMatriz de confusão:')
     print(conf_matrix)
     print("\n")
-
+    
     nv_classes_ = df.get_classes()
-    plot_confusion_matrix(conf_matrix, nv_classes_)
+    graficos.plot_confusion_matrix(conf_matrix, nv_classes_)
