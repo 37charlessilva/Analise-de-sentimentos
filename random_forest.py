ac3b6b6c30e06324fca26d3b72fdd4125a9afe1e
Charles Silva <37charlessilva@gmail.com>
2024-06-27 11:18:07 -0300

    add random_forest

--- random_forest.py
+++ random_forest.py
@@ -1,16 +1,16 @@
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import time
 
-class SVM_model:
-    def __init__(self, kernel='linear', C=1.0) -> None:
+class RandomForest:
+    def __init__(self) -> None:
         self.train_x = None
         self.test_x = None
         self.train_y = None
         self.test_y = None
         self.test_y_pred = None
         self.training_time = None
-        self.svm_model = SVC(kernel=kernel, C=C)
+        self.rf_model = RandomForestClassifier(n_estimators=50, random_state=42)
         
     def start(self, train_x, test_x, train_y, test_y):
         # Temos os conjuntos de teste e treinamento divididos
@@ -18,17 +18,17 @@ class SVM_model:
         self.test_x = test_x
         self.train_y = train_y
         self.test_y = test_y
-        
-        # Treinar o modelo SVM
+
+        # Treinar o modelo Random Forest
         start_time = time.time()
-        self.svm_model.fit(self.train_x, self.train_y)
+        self.rf_model.fit(train_x, train_y)
         end_time = time.time()
-        
+
         self.training_time = (end_time - start_time) / 60
-        print(f"Tempo de treinamento do SVM: {self.training_time} minutos\n")
+        print(f"Tempo de treinamento do random forest: {self.training_time} minutos\n")
 
         # Fazer previsões no conjunto de teste
-        self.test_y_pred = self.svm_model.predict(self.test_x)
+        self.test_y_pred = self.rf_model.predict(test_x)
 
     def get_test_y_pred(self):
         return self.test_y_pred
@@ -42,7 +42,7 @@ class SVM_model:
         return accuracy_score(self.test_y, self.test_y_pred)
 
     def classification_report(self):
-        # Retorna um relatório detalhado das métricas de classificação.
+        # Retorna um relatório detalhado das métricas de classificação
         return classification_report(self.test_y, self.test_y_pred, output_dict=True)
     
     def confusion_matrix(self):
